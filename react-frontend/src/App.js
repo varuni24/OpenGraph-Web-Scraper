ef9cd3a77c1f9bf40cc9c868b66c7ccc7fd740ff
Marcus L <mluong2103@gmail.com>
2023-07-21 21:51:16 -0700

    adding documentation

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,6 +1,45 @@
 import React, { useState } from 'react';
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import Documentation from "./documentation.js";
 
-function App() {
+export default function App() {
+  return (
+    <div>
+      <h1>cool website!!</h1>
+
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="documentation" element={<Documentation />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/documentation">Documentation</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+      <Outlet />
+    </div>
+  );
+}
+
+function Home() {
   const [URL, setURL] = useState("");
   const [extractedOGData, setExtractedOGData] = useState(null)
   const [clickedSearch, setClickedSearch] = useState(null);
@@ -53,4 +92,13 @@ function App() {
   );
 }
 
-export default App;
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
