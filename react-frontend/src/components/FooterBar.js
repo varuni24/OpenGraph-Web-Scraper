6448b964c60a5c5e1b1eb281d3fdebe83eecc8e7
ddavidhe <davidhe.3612@gmail.com>
2023-07-29 15:09:21 -0400

    updated tryme sizing

--- react-frontend/src/components/FooterBar.js
+++ react-frontend/src/components/FooterBar.js
@@ -6,11 +6,7 @@ const FooterBar = () => {
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
           <div className="mt-3">
-<<<<<<< HEAD
               Made by Daniel, David, Dylan, Marcus, and Varuni
-=======
-            Made by Dylan, Daniel, Varuni, David, and Marcus
->>>>>>> 0fe0f04a8efb0d0089e4c6aa90068d7e4908fb2f
           </div>
           <div className="text-center text-sm">
             For any feedback, contact xyz@gmail.com
